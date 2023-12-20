03d66dfc9531d3edb4ba0010ad1d0eae8dccd5b9
dudeurv <144495312+dudeurv@users.noreply.github.com>
2023-12-20 02:03:40 +0000

    Update dataset_BraTS.py

--- dataset_BraTS.py
+++ dataset_BraTS.py
@@ -53,7 +53,7 @@ class RandomGenerator(object):
         return sample
 
 
-class Synapse_dataset(Dataset):
+class BraTS_dataset(Dataset):
     def __init__(self, base_dir, list_dir, split, transform=None):
         self.transform = transform  # using transform in torch!
         self.split = split
