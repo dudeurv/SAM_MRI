33653e275c5ec6169ef339b5f0628392a3edfe62
dudeurv <144495312+dudeurv@users.noreply.github.com>
2023-12-20 03:08:43 +0000

    Update dataset_BraTS.py

--- dataset_BraTS.py
+++ dataset_BraTS.py
@@ -1,56 +1,9 @@
-import os
-import random
-import h5py
+from torch.utils.data import Dataset, DataLoader
+from glob import glob
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+from glob import glob
 import numpy as np
-import torch
-from scipy import ndimage
-from scipy.ndimage.interpolation import zoom
-from torch.utils.data import Dataset
-from einops import repeat
-from icecream import ic
-
-
-def random_rot_flip(image, label):
-    k = np.random.randint(0, 4)
-    image = np.rot90(image, k)
-    label = np.rot90(label, k)
-    axis = np.random.randint(0, 2)
-    image = np.flip(image, axis=axis).copy()
-    label = np.flip(label, axis=axis).copy()
-    return image, label
-
-
-def random_rotate(image, label):
-    angle = np.random.randint(-20, 20)
-    image = ndimage.rotate(image, angle, order=0, reshape=False)
-    label = ndimage.rotate(label, angle, order=0, reshape=False)
-    return image, label
-
-
-class RandomGenerator(object):
-    def __init__(self, output_size, low_res):
-        self.output_size = output_size
-        self.low_res = low_res
-
-    def __call__(self, sample):
-        image, label = sample['image'], sample['label']
-
-        if random.random() > 0.5:
-            image, label = random_rot_flip(image, label)
-        elif random.random() > 0.5:
-            image, label = random_rotate(image, label)
-        x, y = image.shape
-        if x != self.output_size[0] or y != self.output_size[1]:
-            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
-            label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
-        label_h, label_w = label.shape
-        low_res_label = zoom(label, (self.low_res[0] / label_h, self.low_res[1] / label_w), order=0)
-        image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
-        image = repeat(image, 'c h w -> (repeat c) h w', repeat=3)
-        label = torch.from_numpy(label.astype(np.float32))
-        low_res_label = torch.from_numpy(low_res_label.astype(np.float32))
-        sample = {'image': image, 'label': label.long(), 'low_res_label': low_res_label.long()}
-        return sample
 
 def normalise_intensity(image, ROI_thres=0.1):
     pixel_thres = np.percentile(image, ROI_thres)
@@ -68,8 +21,8 @@ def map_labels(label):
     return mapped_label
 
 class BratsDataset(Dataset):
-    def __init__(self, root):
-        self.img_path_all = glob(root + '/BraTS-GLI-t1c/*.png')
+    def __init__(self, base_dir):
+        self.img_path_all = glob(base_dir + '/BraTS-GLI-t1c/*.png')
         self.mask_path_all = [img_path.replace('t1c', 'seg') for img_path in self.img_path_all]
 
     def __len__(self):
@@ -80,4 +33,5 @@ class BratsDataset(Dataset):
         image = normalise_intensity(image)
         label = iio.imread(self.mask_path_all[index])
         label = map_labels(label)
-        return image, label
+        sample = {'image': image, 'label': label}
+        return sample
